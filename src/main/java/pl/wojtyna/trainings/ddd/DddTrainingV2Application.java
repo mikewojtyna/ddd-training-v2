9d354e2488ff1e07a7dfe1977d1b76f8fc3e62df
Mike Wojtyna <mike@buildmysoftware.pro>
2023-04-16 10:55:42 +0200

    Init project

---
+++ src/main/java/pl/wojtyna/trainings/ddd/DddTrainingV2Application.java
@@ -0,0 +1,13 @@
+package pl.wojtyna.trainings.ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddTrainingV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddTrainingV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wojtyna/trainings/ddd/DddTrainingV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojtyna.trainings.ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddTrainingV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
