ccbebb426c83199d97bc19a7299911d9ffdb75ac
Mike Wojtyna <mike@wojtyna.pl>
2023-09-23 18:40:36 +0200

    Remove misleading verified payments event from deposit payment processor

--- src/main/java/pl/wojtyna/trainings/ddd/crowdsorcery/deposit/domain/payment/PaymentProcessor.java
+++ src/main/java/pl/wojtyna/trainings/ddd/crowdsorcery/deposit/domain/payment/PaymentProcessor.java
@@ -3,8 +3,6 @@ package pl.wojtyna.trainings.ddd.crowdsorcery.deposit.domain.payment;
 import org.jmolecules.architecture.hexagonal.SecondaryPort;
 import pl.wojtyna.trainings.ddd.crowdsorcery.deposit.domain.account.Deposit;
 
-import java.util.concurrent.Flow;
-
 @SecondaryPort
 public interface PaymentProcessor {
 
