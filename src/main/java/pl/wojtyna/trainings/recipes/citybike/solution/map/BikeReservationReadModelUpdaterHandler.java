b2232bc98d1ff837d073f19f6955becfc67082ce
Mike Wojtyna <mike@wojtyna.pl>
2023-10-04 13:19:33 +0300

    fix readmodel switch case

--- src/main/java/pl/wojtyna/trainings/recipes/citybike/solution/map/BikeReservationReadModelUpdaterHandler.java
+++ src/main/java/pl/wojtyna/trainings/recipes/citybike/solution/map/BikeReservationReadModelUpdaterHandler.java
@@ -9,20 +9,32 @@ public class BikeReservationReadModelUpdaterHandler {
 
     private MapReadDatabase mapReadDatabase;
 
+    public BikeReservationReadModelUpdaterHandler(MapReadDatabase mapReadDatabase) {
+        this.mapReadDatabase = mapReadDatabase;
+    }
+
     @EventListener
     public void handle(BikeReservationPublicEvent event) {
         switch (event) {
-            case BikeReservationPublicEvent.BikeRented bikeRented -> {
-                // update read model
-                mapReadDatabase.readData().getBikeStations().stream().filter(station -> station.getId().equals(bikeRented.stationId())).findAny().ifPresent(station -> {
-                    station.getBikes().stream().filter(bike -> bike.getId().equals(bikeRented.bikeId())).findAny()
-                           .ifPresent(bike -> {
-                               bike.setAvailable(false);
-                           });
-                    case BikeReservationPublicEvent.BikeReturned bikeReturned -> {
-                        // update read model
-                    }
-                }
+            case BikeReservationPublicEvent.BikeRented bikeRented ->
+                mapReadDatabase.readData().getBikeStations().stream()
+                               .filter(station -> station.getId().equals(bikeRented.stationId())).findAny().ifPresent(
+                                   station -> {
+                                       station.getBikes().stream().filter(bike -> bike.getId().equals(bikeRented.bikeId())).findAny()
+                                              .ifPresent(bike -> {
+                                                  bike.setAvailable(false);
+                                              });
+                                   });
+            case BikeReservationPublicEvent.BikeReturned bikeReturned -> {
+                mapReadDatabase.readData().getBikeStations().stream()
+                               .filter(station -> station.getId().equals(bikeReturned.stationId())).findAny().ifPresent(
+                                   station -> {
+                                       station.getBikes().stream().filter(bike -> bike.getId().equals(bikeReturned.bikeId())).findAny()
+                                              .ifPresent(bike -> {
+                                                  bike.setAvailable(true);
+                                              });
+                                   });
             }
         }
     }
+}
